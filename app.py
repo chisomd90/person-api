b01f72b59822c7ed9514306dd849c1252ea37fde
Stoney <chisomd90@gmail.com>
2023-09-15 22:40:49 +0100

    fix and change

--- app.py
+++ app.py
@@ -1,57 +1,67 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from utils import convertListToObject
+import sqlite3
 
-db = SQLAlchemy()
+connect = sqlite3.connect('database.db')
+connect.execute(
+    'CREATE TABLE IF NOT EXISTS Persons (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age TEXT)')
+
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-
-class Person(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.Integer)
 
 # Create a new person
 @app.route('/api', methods=['POST'])
 def create_person():
     data = request.json
-    new_person = Person(name=data['name'], age=data['age'])
-    db.session.add(new_person)
-    db.session.commit()
+    name = data["name"]
+    age=data["age"]
+    with sqlite3.connect("database.db") as users:
+         cursor = users.cursor()
+         cursor.execute("INSERT INTO Persons \
+                        (name, age) VALUES (?,?)", (name, age))
+         print(users.commit())
     return jsonify({'message': 'Person created successfully'}), 201
 
 # Read a person by ID
 @app.route('/api/<int:user_id>', methods=['GET'])
 def get_person(user_id):
-    person = Person.query.get_or_404(user_id)
-    return jsonify({'name': person.name, 'age': person.age})
+    # id = int(user_id)
+    connect = sqlite3.connect('database.db')
+    cursor = connect.cursor()
+    cursor.execute("SELECT cast(id as integer) FROM Persons WHERE id = id")
+    person = cursor.fetchall()
+    print(person)
+    # result = list(map(convertListToObject, person))
+    return jsonify({"result": 'result'}), 200
 
-# Read a person by ID
+# Get all persons
 @app.route('/api/', methods=['GET'])
 def get_all_persons():
-    person = Person.query
-    print(person)
-    return jsonify({'test': 'true'})
+    connect = sqlite3.connect('database.db')
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM Persons")
+    person = cursor.fetchall()
+    result = list(map(convertListToObject, person))
+    return jsonify({"result": result}), 200
 
 # Update a person by ID
 @app.route('/api/<int:user_id>', methods=['PUT'])
 def update_person(user_id):
-    person = Person.query.get_or_404(user_id)
+    # person = Person.query.get_or_404(user_id)
     data = request.json
-    person.name = data['name']
-    person.age = data['age']
-    db.session.commit()
+    # person.name = data['name']
+    # person.age = data['age']
+    # db.session.commit()
     return jsonify({'message': 'Person updated successfully'})
 
 # Delete a person by ID
 @app.route('/api/<int:user_id>', methods=['DELETE'])
 def delete_person(user_id):
-    person = Person.query.get_or_404(user_id)
-    db.session.delete(person)
-    db.session.commit()
+    # person = Person.query.get_or_404(user_id)
+    # db.session.delete(person)
+    # db.session.commit()
     return jsonify({'message': 'Person deleted successfully'})
 
 def __init__(self, name, age):
@@ -59,6 +69,7 @@ def __init__(self, name, age):
         self.age = age
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+        # db.create_all()
+
     app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,7 @@
+def convertListToObject(item):
+    print(item)
+    return {
+        "id":item[0],
+        "name": item[1],
+        "age": item[2]
+    }
